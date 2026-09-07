0816c9f319a37d9b3fca2617953a63463c4afdc2
Reece-Bygrave <reecebygravedev@gmail.com>
2026-09-07 19:16:11 +0100

    bug fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
         </Link>
 
         <p className="text-base sm:text-xl md:text-2xl font-semibold text-slate-800 dark:text-slate-200 mb-4 transition-colors">
-          Educator | Developer
+          BSc Computer Science Graduate & Educator | Developer
         </p>
 
         {/* Profile Summary */}
